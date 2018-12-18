e5dceb1921fe88fefec6e148119026f0caaa0c01
Bryce DesBrisay <bryce@desbrisay.com>
2018-12-18 05:10:55 -0800

    cleaned code, added custom styles and default props

--- src/demo/App.js
+++ src/demo/App.js
@@ -5,10 +5,30 @@ const App = () => (
   <div>
     <Calendar 
       onChange={(val) => {console.log(val)}}
-      selectedDate={new Date('Jan 18 2018')}
-      selectedMonth={new Date('Jan 18 2018')}
-      minDate={new Date('Jan 14 2018')}
-      maxDate={new Date('Jan 32 2018')}
+      selectedDate={new Date('Dec 18 2018')}
+      selectedMonth={new Date('Dec 18 2018')}
+      eventDates={[
+        new Date('Dec 13 2018'),
+        new Date('Dec 21 2018'),
+        new Date('Dec 24 2018')
+      ]}
+      minDate={new Date('Dec 10 2018')}
+      maxDate={new Date('Jan 3 2019')}
+      customStyles={{
+        header: { paddingBottom: 10 },
+        title: { textTransform: 'uppercase' },
+        prev: { paddingLeft: 30, color: 'blue' },
+        next: { paddingRight: 30, color: 'lightblue' },
+        daysRow: { background: '#bbb' },
+        days: { color: 'white' },
+        cell: { paddingTop: 10 },
+        innerCell: { borderRadius: 5 },
+        number: { fontSize: 16 },
+        selected: { background: 'purple' },
+        event: { background: 'teal', color: 'white' },
+        extra: { color: 'green' },
+        outOfRange: { color: 'red', fontWeight: '400' }
+      }}
     />
   </div>
 );
--- src/lib/components/Calendar.js
+++ src/lib/components/Calendar.js
@@ -9,23 +9,38 @@ const isInArray = (array, date) => (
 
 class Calendar extends React.Component {
   state = {
-    selectedMonth: this.props.selectedMonth ? this.props.selectedMonth : new Date(),
-    selectedDate: this.props.selectedDate ? this.props.selectedDate : new Date(),
-    eventDates: this.props.eventDates ? this.props.eventDates : []
+    selectedMonth: this.props.selectedMonth,
+    selectedDate: this.props.selectedDate,
+    eventDates: this.props.eventDates
   };
 
   renderHeader() {
-    const dateFormat = "MMMM YYYY";
+    const label = dateFns.format(this.state.selectedMonth, 'MMMM YYYY')
+    const { customStyles } = this.props;
 
     return (
-      <div className="header row flex-middle">
-        <div className="col col-start" onClick={this.prevMonth}>
+      <div 
+        className="header row" 
+        style={customStyles.header}
+      >
+        <div 
+          className="col col-start"
+          style={customStyles.prev} 
+          onClick={this.prevMonth}
+        >
           <div className="icon">chevron_left</div>
         </div>
-        <div className="col col-center">
-          <span>{dateFns.format(this.state.selectedMonth, dateFormat)}</span>
+        <div 
+          className="col col-center"
+          style={customStyles.title}
+        >
+          <span>{label}</span>
         </div>
-        <div className="col col-end" onClick={this.nextMonth}>
+        <div 
+          className="col col-end"
+          style={customStyles.next}
+          onClick={this.nextMonth}
+        >
           <div className="icon">chevron_right</div>
         </div>
       </div>
@@ -33,70 +48,83 @@ class Calendar extends React.Component {
   }
 
   renderDays() {
-    const dateFormat = "ddd";
+    const { customStyles } = this.props;
+    const startDate = dateFns.startOfWeek(this.state.selectedMonth);
     const days = [];
 
-    let startDate = dateFns.startOfWeek(this.state.selectedMonth);
-
     for (let i = 0; i < 7; i++) {
+      const label = dateFns.format(dateFns.addDays(startDate, i), 'ddd');
+
       days.push(
-        <div className="col col-center" key={i}>
-          {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+        <div 
+          key={i}
+          className="col col-center"
+          style={customStyles.days}
+        >
+          {label}
         </div>
       );
     }
 
-    return <div className="days row">{days}</div>;
+    return <div className="days row" style={customStyles.daysRow}>{days}</div>;
   }
 
   renderCells() {
     const { selectedMonth, selectedDate, eventDates } = this.state;
-    const { minDate, maxDate } = this.props;
+    const { minDate, maxDate, customStyles } = this.props;
     const monthStart = dateFns.startOfMonth(selectedMonth);
     const monthEnd = dateFns.endOfMonth(monthStart);
     const startDate = dateFns.startOfWeek(monthStart);
     const endDate = dateFns.endOfWeek(monthEnd);
-
-    const dateFormat = "D";
     const rows = [];
-
     let days = [];
     let day = startDate;
-    let formattedDate = "";
 
     while (day <= endDate) {
       for (let i = 0; i < 7; i++) {
-        formattedDate = dateFns.format(day, dateFormat);
+        const formattedDate = dateFns.format(day, 'D');
         const cloneDay = day;
+
+        let outerClass = 'col cell';
+        let outerStyle = customStyles.cell;
+        if (!dateFns.isSameMonth(day, monthStart)) {
+          outerClass += ' disabled';
+          outerStyle = {...outerStyle, ...customStyles.extra};
+        }
+        if (dateFns.isBefore(day, minDate) || dateFns.isAfter(day, maxDate)) {
+          outerClass += ' disabled outside';
+          outerStyle = {...outerStyle, ...customStyles.extra, ...customStyles.outOfRange };
+        }
+
+        let innerClass = 'innerCell';
+        let innerStyle = customStyles.innerCell;
+        if (isInArray(eventDates, day)) {
+          innerClass += ' event';
+          innerStyle = {...innerStyle, ...customStyles.event};
+        }
+        if (dateFns.isSameDay(day, selectedDate)) {
+          innerClass += ' selected';
+          innerStyle = {...innerStyle, ...customStyles.selected};
+        }
 
         days.push(
           <div
-            className={`col cell${
-              !dateFns.isSameMonth(day, monthStart) ? ' disabled' : ''
-            }${
-              (dateFns.isBefore(day, minDate) || dateFns.isAfter(day, maxDate)) ? ' disabled outside' : ''
-            }`}
             key={day}
+            className={outerClass}
+            style={outerStyle}
             onClick={() => this.onDateClick(dateFns.parse(cloneDay))}
           >
-            <div 
-              className={`innerCell${
-                isInArray(eventDates, day) ? ' event' : ''
-              }${
-                dateFns.isSameDay(day, selectedDate) ? ' selected' : ''
-              }`}
-            >
-              <span className="number">{formattedDate}</span>
+            <div className={innerClass} style={innerStyle}>
+              <span className="number" style={customStyles.number}>{formattedDate}</span>
             </div>
           </div>
         );
+
         day = dateFns.addDays(day, 1);
       }
 
       rows.push(
-        <div className="row" key={day}>
-          {days}
-        </div>
+        <div className="row" key={day}>{days}</div>
       );
 
       days = [];
@@ -125,6 +153,30 @@ class Calendar extends React.Component {
         {this.renderCells()}
       </div>
     );
+  }
+}
+
+Calendar.defaultProps = {
+  onChange: () => {},
+  selectedMonth: new Date(),
+  selectedDate: new Date(),
+  eventDates: [],
+  minDate: undefined,
+  maxDate: undefined,
+  customStyles: {
+    header: {},
+    title: {},
+    prev: {},
+    next: {},
+    days: {},
+    daysRow: {},
+    cell: {},
+    innerCell: {},
+    number: {},
+    selected: {},
+    event: {},
+    disabled: {},
+    outOfRange: {}
   }
 }
 
