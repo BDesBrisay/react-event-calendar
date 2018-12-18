789c3af1735ddd0c2a08b15b208997c7e6690874
Bryce DesBrisay <bryce@desbrisay.com>
2018-12-17 21:20:22 -0700

    updated disabled and out of range colors

--- src/demo/App.js
+++ src/demo/App.js
@@ -7,8 +7,8 @@ const App = () => (
       onChange={(val) => {console.log(val)}}
       selectedDate={new Date('Jan 18 2018')}
       selectedMonth={new Date('Jan 18 2018')}
-      minDate={new Date('Jan 17 2018')}
-      maxDate={new Date('Jan 20 2018')}
+      minDate={new Date('Jan 14 2018')}
+      maxDate={new Date('Jan 32 2018')}
     />
   </div>
 );
